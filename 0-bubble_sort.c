244537d08ec8ddfbd1e269c47254519eb54f860c
Ardiy <you@example.com>
2023-07-23 23:32:02 +0000

    addition of knuth sequence calculation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,24 +5,32 @@
 /**
  * bubble_sort - function that implements the bubble sort
  * algorithm
- * @array: The array to be printed
+ * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
 	sorting_validation(array, size);
 
 	for (i = 0; i < size - 1; i++)
 	{
+	swapped = 0;
+
 	for (j = 0; j < size - i - 1; j++)
 	{
 		if (array[j] > array[j + 1])
 		{
 		swap(&array[j], &array[j + 1]);
-		print_array(array, size);
+		swapped = 1;
 		}
 	}
+
+	if (swapped == 0)
+		break;
+
+	print_array(array, size);
 	}
 }
