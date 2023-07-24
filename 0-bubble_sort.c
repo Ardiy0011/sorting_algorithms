e80c73a8c47c9e18db37ebbfb8bc7e42ce0654af
Ardiy <you@example.com>
2023-07-24 19:00:53 +0000

    validation fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,16 @@
 #include "sort.h"
-#include "swap.c"
+
+/**
+ * swap - function that sways the positions of 2 integers
+ * @first_int: pointer to the firist integer
+ * @second_int: pointer to the 2nd integer
+ */
+void swap(int *first_int, int *second_int)
+{
+    int temp = *first_int;
+    *first_int = *second_int;
+    *second_int = temp;
+}
 
 /**
  * bubble_sort - function that implements the bubble sort
