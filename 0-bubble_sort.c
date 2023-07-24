ff7aa4cee1b9a54f69cb643694266468e48ec9d6
Ardiy <you@example.com>
2023-07-24 17:59:21 +0000

    validation fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,27 +2,30 @@
 #include "swap.c"
 
 /**
-* bubble_sort - Sorts array using bubble sort
-* @array: The array to be sorted
-* @size: Number of elements in the array
-* Return: void
-*/
+ * bubble_sort - function that implements the bubble sort
+ * algorithm
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ *
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (!array || size < 2)
-		return;
+        if (array == NULL || size <= 1)
+        return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+	for (j = 0; j < size - i - 1; j++)
+	{
+		if (array[j] > array[j + 1])
 		{
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
-			}
+		swap(&array[j], &array[j + 1]);
+		print_array(array, size);
+
 		}
+	}
 	}
 }
