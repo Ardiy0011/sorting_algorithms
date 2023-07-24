e60aa31fcab3de985f1c66631cf5f2f1c2fc24b0
Ardiy <you@example.com>
2023-07-24 00:44:26 +0000

    addition of knuth sequence calculation

--- sorting_validation.c
+++ sorting_validation.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * sorting_validation - checks if list is sorted
+ * algorithm
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
 void sorting_validation(int *array, size_t size)
 {
 	size_t i;
