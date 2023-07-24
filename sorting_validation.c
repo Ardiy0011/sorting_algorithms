4b8235657d0427bc280bfe08db28c44bcd0cb1b8
Ardiy <you@example.com>
2023-07-24 16:37:39 +0000

    validation fix

--- sorting_validation.c
+++ sorting_validation.c
@@ -10,7 +10,12 @@ void sorting_validation(int *array, size_t size)
 {
 	size_t i;
 
-        if (!array || size < 2)
-        return;
+	for (i = 0; i < size - 1; i++)
+	{
+	if (array[i] > array[i + 1])
+	{
+		break;
+	}
+	}
 
 }
