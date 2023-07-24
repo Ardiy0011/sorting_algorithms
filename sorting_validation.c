396e4fcd1ef2383b0804a84ca94e39d734e34ae6
Ardiy <you@example.com>
2023-07-24 17:02:32 +0000

    validation fix

--- sorting_validation.c
+++ sorting_validation.c
@@ -6,11 +6,10 @@
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
-void sorting_validation(int *array, int size)
+void sorting_validation(int *array, size_t size)
 {
 
         if (!array || size < 2)
-        printf("empty array");
         return;
 
 }
