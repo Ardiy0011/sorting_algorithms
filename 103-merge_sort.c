02ad8b008dded857076994f98913802f42b32aa6
Ardiy <you@example.com>
2023-07-25 05:06:26 +0000

    merge sort attempt

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,51 +10,51 @@
  */
 void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
 {
-    size_t i = 0, j = 0, k = 0;
-    int *temp = (int *)malloc((left_size + right_size) * sizeof(int));
+	size_t i = 0, j = 0, k = 0;
+	int *temp = (int *)malloc((left_size + right_size) * sizeof(int));
 
-    if (temp == NULL)
-    {
-        printf("Memory allocation error!\n");
-        return;
-    }
+	if (temp == NULL)
+	{
+	printf("Memory allocation error!\n");
+	return;
+	}
 
-    while (k < left_size + right_size)
-    {
-        switch ((i < left_size) * 2 + (j < right_size))
-        {
-            case 3:
-                if (left[i] <= right[j])
-                {
-                    temp[k] = left[i];
-                    i++;
-                }
-                else
-                {
-                    temp[k] = right[j];
-                    j++;
-                }
-                break;
+	while (k < left_size + right_size)
+	{
+	switch ((i < left_size) * 2 + (j < right_size))
+	{
+		case 3:
+		if (left[i] <= right[j])
+		{
+			temp[k] = left[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = right[j];
+			j++;
+		}
+		break;
 
-            case 2:
-                temp[k] = left[i];
-                i++;
-                break;
+		case 2:
+		temp[k] = left[i];
+		i++;
+		break;
 
-            case 1:
-                temp[k] = right[j];
-                j++;
-                break;
-        }
-        k++;
-    }
+		case 1:
+		temp[k] = right[j];
+		j++;
+		break;
+	}
+	k++;
+	}
 
-    for (i = 0; i < left_size + right_size; i++)
-    {
-        array[i] = temp[i];
-    }
+	for (i = 0; i < left_size + right_size; i++)
+	{
+	array[i] = temp[i];
+	}
 
-    free(temp);
+	free(temp);
 }
 
 /**
