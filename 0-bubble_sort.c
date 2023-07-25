15e4d0c11ae7675afc386caffb6ced6064cb956e
Segun Adeleke <oadeleke123@gmail.com>
2023-07-25 10:51:13 +0100

    fix: indentation using tabs (betty compliance)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,18 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-        if (array == NULL || size <= 1)
-        return;
+	if (array == NULL || size <= 1)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j < size - 1; j++)
-	{
-		if (array[j] > array[j + 1])
+		for (j = 0; j < size - 1; j++)
 		{
-		swap(&array[j], &array[j + 1]);
-		print_array(array, size);
-
+			if (array[j] > array[j + 1])
+			{
+			swap(&array[j], &array[j + 1]);
+			print_array(array, size);
+			}
 		}
-	}
 	}
 }
