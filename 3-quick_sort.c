661d797154711d651b575f609edd85b4bd103438
Ardiy <you@example.com>
2023-07-25 02:32:40 +0000

    lomuto application refinement

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,7 +26,7 @@ size_t lomuto_partition(int *array, ssize_t before, ssize_t after, size_t size)
 		}
 	}
 
-	if (i != after)
+	if (i + 1 != after)
 	{
 		swap(&array[i + 1], &array[after]);
 		print_array(array, size);
