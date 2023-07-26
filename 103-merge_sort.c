82938d2b0d9c2edfa1722329e1f5ad93a365dd3d
Ardiy <you@example.com>
2023-07-26 11:19:30 +0000

    test case for merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insert - Function that implements the insertion sort algorithm.
+ * insertion_sort - Function that implements the insertion sort algorithm.
  * @array: The input array.
  * @size: Number of elements in @array.
  */
@@ -25,6 +25,7 @@ void insert(int *array, size_t size)
 	}
 }
 
+
 /**
  * merge - Merge two sorted subarrays into a single sorted array.
  * @array: The input array.
@@ -40,44 +41,45 @@ void merge(int *array, int *left, size_t lz, int *right, size_t rz)
 
 	if (temp == NULL)
 	{
-		return;
+	printf("Memory allocation error!\n");
+	return;
 	}
 	while (k < lz + rz)
 	{
-		switch ((i < lz) * 2 + (j < rz))
+	switch ((i < lz) * 2 + (j < rz))
+	{
+		case 3:
+		if (left[i] <= right[j])
 		{
-			case 3:
-				if (left[i] <= right[j])
-				{
-					temp[k] = left[i];
-					i++;
-				}
-				else
-				{
-					temp[k] = right[j];
-					j++;
-				}
-				break;
-			case 2:
-				temp[k] = left[i];
-				i++;
-				break;
-			case 1:
-				temp[k] = right[j];
-				j++;
-				break;
+			temp[k] = left[i];
+			i++;
 		}
-		k++;
+		else
+		{
+			temp[k] = right[j];
+			j++;
+		}
+		break;
+		case 2:
+		temp[k] = left[i];
+		i++;
+		break;
+		case 1:
+		temp[k] = right[j];
+		j++;
+		break;
+	}
+	k++;
 	}
 	for (i = 0; i < lz + rz; i++)
 	{
-		array[i] = temp[i];
+	array[i] = temp[i];
 	}
 	free(temp);
 }
 
 /**
- * merge_sort - Function that implements the top-down merge sort algorithm.
+ * merge_sort - Function that implements the merge sort algorithm
  * @array: The input array.
  * @size: Number of elements in @array.
  */
@@ -85,30 +87,24 @@ void merge_sort(int *array, size_t size)
 {
 	if (size > 1)
 	{
-		if (size <= 10)
+			if (size <= 10)
 		{
-			insertion_sort(array, size);
+			insert(array, size);
 			print_array(array, size);
 			return;
 		}
+	size_t mid = size / 2;
+	size_t lz = mid;
+	size_t rz = size - mid;
 
-		size_t mid = size / 2;
-		size_t lz = mid;
-		size_t rz = size - mid;
+	int *left = array;
+	int *right = array + mid;
 
-		int *left = array;
-		int *right = array + mid;
+	merge_sort(left, lz);
+	merge_sort(right, rz);
 
-		merge_sort(left, lz);
-		merge_sort(right, rz);
+	merge(array, left, lz, right, rz);
 
-		if (left[lz - 1] <= right[0])
-		{
-			return;
-		}
-
-		merge(array, left, lz, right, rz);
-
-		print_array(array, size);
+	print_array(array, size);
 	}
 }
