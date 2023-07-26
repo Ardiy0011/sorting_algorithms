c5b796864453458dbdb17a2f06564d625d2a4f8c
Ardiy <you@example.com>
2023-07-26 10:37:52 +0000

    test case for merge sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,17 +4,17 @@
 /**
  * lomuto_partition - Lomuto partition scheme for quicksort
  * @array: The input array
- * @before: The beforeer bound index
- * @after: The afterer bound index
+ * @be: The beforer bound index
+ * @af: The afer bound index
  * @size: Number of elements in @array
- * Return: Index of the pivot after partitioning
+ * Return: Index of the pivot af partitioning
  */
-size_t lomuto_partition(int *array, ssize_t before, ssize_t after, size_t size)
+size_t lomuto_seq(int *array, ssize_t be, ssize_t af, size_t size)
 {
-	int pivot = array[after];
-	ssize_t i = before - 1, j;
+	int pivot = array[af];
+	ssize_t i = be - 1, j;
 
-	for (j = before; j < after; j++)
+	for (j = be; j < af; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -27,9 +27,9 @@ size_t lomuto_partition(int *array, ssize_t before, ssize_t after, size_t size)
 		}
 	}
 
-	if (i + 1 != after)
+	if (i + 1 != af)
 	{
-		swap(&array[i + 1], &array[after]);
+		swap(&array[i + 1], &array[af]);
 		print_array(array, size);
 	}
 
@@ -39,18 +39,18 @@ size_t lomuto_partition(int *array, ssize_t before, ssize_t after, size_t size)
 /**
  * quick_sort_recursion - Recursive recursion function for quicksort
  * @array: The input array
- * @before: The beforeer bound index
- * @after: The afterer bound index
+ * @be: The beforer bound index
+ * @af: The after bound index
  * @size: Number of elements in @array
  */
-void quick_sort_recursion(int *array, ssize_t before, ssize_t after, size_t size)
+void qs_recursion(int *array, ssize_t be, ssize_t af, size_t size)
 {
-	if (before < after)
+	if (be < af)
 	{
-	size_t pivot_idx = lomuto_partition(array, before, after, size);
+	size_t pivot_idx = lomuto_partition(array, be, af, size);
 
-	quick_sort_recursion(array, before, pivot_idx - 1, size);
-	quick_sort_recursion(array, pivot_idx + 1, after, size);
+	quick_sort_recursion(array, be, pivot_idx - 1, size);
+	quick_sort_recursion(array, pivot_idx + 1, af, size);
 	}
 }
 
